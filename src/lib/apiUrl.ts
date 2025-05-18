b698f2cca665a28894bd4443334f541bc6cd1214
AbayoHJovin <abayohirwajovin@gmail.com>
2025-05-18 22:14:31 +0200

    apiurl added

--- src/lib/apiUrl.ts
+++ src/lib/apiUrl.ts
@@ -1,6 +1,3 @@
 /* eslint-disable no-undef */
 export const prod = process.env.NODE_ENV === "production";
-
-// In development, use relative URLs to leverage the proxy
-// In production, use the full URL
-export const apiUrl = prod ? "https://homedel.onrender.com" : "";
+export const apiUrl = prod ? "https://citizen-voice-pdca.onrender.com" : "";
